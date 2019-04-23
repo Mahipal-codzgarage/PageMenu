c02776cea943600f56cf12bf3c1db20d0da3cee4
Mahipal <mahipal.thakor5626@gmail.com>
2019-04-23 13:28:29 +0530

    minor change

--- Classes/CAPSPageMenu.swift
+++ Classes/CAPSPageMenu.swift
@@ -83,10 +83,11 @@ open class CAPSPageMenu: UIViewController {
      */
     public init(viewControllers: [UIViewController], frame: CGRect, options: [String: AnyObject]?) {
         super.init(nibName: nil, bundle: nil)
-        
+        /*
         if configuration.isArabic {
             UIView.appearance().semanticContentAttribute = .forceRightToLeft
         }
+        */
         
         controllerArray = viewControllers
         
